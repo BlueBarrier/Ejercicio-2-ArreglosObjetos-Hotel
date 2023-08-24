9cf8f3f6a60d6f1fc7ba9470d882a048e7898c68
Erick Barrera <dannbj1@gmail.com>
2023-08-24 06:49:02 -0600

    Creación clase reserva

--- Reserva.java
+++ Reserva.java
@@ -1,3 +1,26 @@
+/**
+ * Universidad del Valle DE Guatemala
+ * Departamento de Ciencias de la Computación
+ * Programación Orientada a Objetos - CC2008 - 50
+ * 
+ * @author: Erick Barrera - 231238
+ * @Description: 
+ * @version: 1.0
+ * @Fecha: 24/08/2023
+ * @Fecha_ultima_modificación: 24/08/2023
+ */
 public class Reserva {
-    
+    public Cliente[] listaEspera;
+    public Cliente[] clientes;
+    public Habitacion[] habitaciones;
+    int i = 0; 
+    int j = 0;
+    public void agregarHabitacion(Habitacion habitacion){ // método para agregar una habitación a la lista
+        habitaciones[0+i] = habitacion;
+        i++; 
+    }
+    public void agregarCliente(Cliente cliente){ // agregar clientes a la lista clientes
+        clientes[0+j] = cliente;
+        j++;
+    }
 }
